42737c93198ec3af36f416175384a09d92a2cc32
KJHo <africancc@163.com>
2021-10-19 21:37:25 +0800

    change while loop

--- password.py
+++ password.py
@@ -1,12 +1,12 @@
 x = 3
 password = 'a123456'
 while x > 0:
+	x = x -1
 	password_input = input('请输入密码：')
 	if password_input == password:
 		print('登入成功')
 		break
 	else:
-		x = x -1
 		if x != 0:
 			print('还有' + str(x) + '次机会')
 		else:
