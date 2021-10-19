5e50747d0bd0223c6449466e3259c460373b2190
KJHo <africancc@163.com>
2021-10-19 21:15:37 +0800

    finished password

---
+++ password.py
@@ -0,0 +1,13 @@
+x = 3
+password = 'a123456'
+while x > 0:
+	password_input = input('请输入密码：')
+	if password_input == password:
+		print('登入成功')
+		break
+	else:
+		x = x -1
+		if x != 0:
+			print('还有' + str(x) + '次机会')
+		else:
+			print('请认真回忆密码嚄')
